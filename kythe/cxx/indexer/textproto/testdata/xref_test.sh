e22436c91968fbc8721835b08029ea72180c0058
Justin Buchanan <justinbuchanan@google.com>
2019-01-03 20:44:27 -0800

    moved test script into sh_test

---
+++ kythe/cxx/indexer/textproto/testdata/xref_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+VERIFIER_BIN="external/io_kythe/kythe/cxx/verifier/verifier"
+PB_INDEXER_BIN="kythe/cxx/indexer/proto/indexer"
+PBTXT_INDEXER_BIN="kythe/cxx/indexer/textproto/indexer"
+
+EXAMPLE_PB=kythe/cxx/indexer/textproto/testdata/example.proto
+EXAMPLE_PBTXT=kythe/cxx/indexer/textproto/testdata/example.pbtxt
+
+PB_ENTRIES="$TEST_TMPDIR/pb_entries"
+PBTXT_ENTRIES="$TEST_TMPDIR/pbtxt_entries"
+
+# Index proto file
+$PB_INDEXER_BIN $EXAMPLE_PB > $PB_ENTRIES
+
+# Index textproto file
+$PBTXT_INDEXER_BIN \
+    --text_proto_file=$EXAMPLE_PBTXT \
+    --message_name=example.MyMessage \
+    $EXAMPLE_PB \
+    > $PBTXT_ENTRIES
+
+# Verify both files, ensuring that xrefs work.
+cat "$PB_ENTRIES" "$PBTXT_ENTRIES" | \
+    "$VERIFIER_BIN" $EXAMPLE_PBTXT $EXAMPLE_PBTXT \
+    --show_goals \
+    --show_protos \
+    --goal_regex="\s*(?:#|(?://))\-(.*)" 
