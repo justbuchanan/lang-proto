d6139350fd7fe0658c0ddaec167ea4579d744bb7
Justin Buchanan <justinbuchanan@google.com>
2019-01-03 20:30:47 -0800

    it works!

--- xref_test.sh
+++ xref_test.sh
@@ -1,11 +1,21 @@
-#!/usr/bin/env bash -e
+#!/bin/bash -e
 
 bazel build //kythe/cxx/indexer/proto:indexer \
             //kythe/cxx/indexer/textproto:indexer \
             @io_kythe//kythe/cxx/verifier
 
-./bazel-bin/kythe/cxx/indexer/proto/indexer kythe/cxx/indexer/textproto/testdata/example.proto > pb.entries                                                                 
-./bazel-bin/kythe/cxx/indexer/textproto/indexer --text_proto_file kythe/cxx/indexer/textproto/testdata/example.pbtxt --message_name=example.MyMessage kythe/cxx/indexer/textproto/testdata/example.proto > pbtxt.entries                                                                                                                                                                                                                             
+./bazel-bin/kythe/cxx/indexer/proto/indexer \
+    kythe/cxx/indexer/textproto/testdata/example.proto \
+    > pb.entries                                                                 
 
- cat pb.entries pbtxt.entries| /opt/kythe/tools/verifier kythe/cxx/indexer/textproto/testdata/example.pbtxt kythe/cxx/indexer/textproto/testdata/example.proto --show_goals \
---goal_regex="(?:#|(?://))\-(.*)" 
+./bazel-bin/kythe/cxx/indexer/textproto/indexer \
+    --text_proto_file kythe/cxx/indexer/textproto/testdata/example.pbtxt \
+    --message_name=example.MyMessage \
+    kythe/cxx/indexer/textproto/testdata/example.proto \
+    > pbtxt.entries                                                                                                                                                                                                                             
+
+ cat pb.entries pbtxt.entries | \
+    bazel-bin/external/io_kythe/kythe/cxx/verifier/verifier kythe/cxx/indexer/textproto/testdata/example.pbtxt kythe/cxx/indexer/textproto/testdata/example.proto \
+    --show_goals \
+    --show_protos \
+    --goal_regex="\s*(?:#|(?://))\-(.*)" 
